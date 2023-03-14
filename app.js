7f722b4aba667755575986ac2cc59b3eccf59be3
Kaitlyn Chen <kaitlynchen77@gmail.com>
2023-03-13 23:20:47 -0700

    routing for pages

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ liveReloadServer.server.once("connection", () => {
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var signinRouter = require('./routes/signin');
+var votingRouter = require('./routes/voting');
 
 var app = express();
 
@@ -38,6 +40,8 @@ app.use(express.static(__dirname + '/public'));
 
 app.use('/', indexRouter);
 // app.use('/users', usersRouter);
+app.use('/signin', signinRouter)
+app.use('/voting', votingRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -49,7 +53,6 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
