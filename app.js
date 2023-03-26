2068b0ff59881a4beb351bc6706adf4fcbab243d
Bryce Gayan <brycegayan@gmail.com>
2023-03-26 14:45:17 -0700

    Cleaning up project structure

--- app.js
+++ app.js
@@ -1,61 +1,30 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-// live reload code
-
-var livereload = require("livereload");
-var connectLiveReload = require("connect-livereload");
-
-const liveReloadServer = livereload.createServer();
-liveReloadServer.server.once("connection", () => {
-  setTimeout(() => {
-    liveReloadServer.refresh("/");
-  }, 100);
-});
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var signinRouter = require('./routes/signin');
-var votingRouter = require('./routes/voting');
-
-var app = express();
-
-app.use(connectLiveReload());
+const express = require('express');
+const app = express();
+const path = require('path');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(__dirname + '/public'));
 
+app.get('/', (req, res) => {
+res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
 
-app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-app.use('/signin', signinRouter)
-app.use('/voting', votingRouter)
+app.get('/signin', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'signin.html')); 
+});
+
+app.get('/voting', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'voting.html')); 
+});
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
 });
 
 module.exports = app;
