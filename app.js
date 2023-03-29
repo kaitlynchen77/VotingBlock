1f7150d316bf575c50163dbc3dc7e0aa9ef7b3cb
Bryce Gayan <brycegayan@gmail.com>
2023-03-29 09:14:16 -0700

    Deleting error handling and fixing breaking change with sign up button on /voting page

--- app.js
+++ app.js
@@ -11,22 +11,8 @@ nunjucks.configure('views', {
   express: app
 });
 
-// live reload code
-
-// var livereload = require("livereload");
-// var connectLiveReload = require("connect-livereload");
-
-// const liveReloadServer = livereload.createServer();
-// liveReloadServer.server.once("connection", () => {
-//   setTimeout(() => {
-//     liveReloadServer.refresh("/");
-//   }, 100);
-// });
-
 var indexRouter = require('./routes/index');
 var votingRouter = require('./routes/voting');
-
-// app.use(connectLiveReload());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -43,20 +29,5 @@ app.use(express.static(__dirname + '/public'));
 
 app.use('/', indexRouter);
 app.use('/voting', votingRouter)
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
