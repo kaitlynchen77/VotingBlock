fa482783c6c5162b63e8e58653e4333cc6ee8860
Kaitlyn Chen <kaitlynchen77@gmail.com>
2023-03-29 08:53:18 -0700

    remove signin page

--- public/javascript/signin.js
+++
@@ -1,6 +0,0 @@
-const signinButton = document.querySelector('.signinButton');
-
-signinButton.addEventListener('click', () => {
-  console.log("hi");
-  ethereum.request({ method: 'eth_requestAccounts' });
-});
