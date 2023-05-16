2485669479712a984b8a496b842d0634ac0dadcc
Kaitlyn Chen <kaitlynchen77@gmail.com>
2023-05-16 09:12:58 -0700

    add endElection() to smart contract

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -19,6 +19,7 @@ contract Voting {
     struct Group {
         string groupTitle;
         Election[] elections; // fixed-size array of elections
+        Election[] completed;
         address[] members; // Array of member addresses
         address adminAddress; 
     }
@@ -86,9 +87,13 @@ contract Voting {
         // Return the name and vote count for the candidate
         return (election.candidates[candidateIndex].name, election.candidates[candidateIndex].voteCount);
     }
-    /*
     function endElection (uint groupID, uint electionIndex) public {
-        
+        Election[] storage elections = groups[groupID].elections;
+        Election[] storage completed = groups[groupID].completed;
+        completed.push(elections[electionIndex]);
+        for(uint i = electionIndex; i < elections.length-1; i++) {
+            elections[i]=elections[i+1];
+        }
+        elections.pop();
     }
-    */
 }
